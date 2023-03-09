4197c855829b8f29b1dd06f97ed7ac7f32fa0ca9
Arman Kudaibergenov <arman.kudaibergenov.mail@gmail.com>
2023-03-09 16:25:46 +0600

    basic registration

--- lib/src/screens/register_screen.dart
+++ lib/src/screens/register_screen.dart
@@ -6,27 +6,69 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      backgroundColor: const Color(0xFFF3F4F6),
       navigationBar: CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.white,
+        border: const Border(),
         middle: Text('Регистрация'),
       ),
       child: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            CupertinoTextField(
-              placeholder: 'Логин',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(
+                  height: 32,
+                ),
+                CupertinoTextField(
+                  placeholder: 'Логин',
+                  decoration: BoxDecoration(color: CupertinoColors.white),
+                  padding: const EdgeInsetsDirectional.symmetric(
+                      vertical: 20, horizontal: 16),
+                ),
+                Container(
+                  height: 1,
+                  color: Color(0xFFe0E6ED),
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                CupertinoTextField(
+                  placeholder: 'Телефон',
+                  padding: const EdgeInsetsDirectional.symmetric(
+                      vertical: 20, horizontal: 16),
+                ),
+                Container(
+                  height: 1,
+                  color: Color(0xFFe0E6ED),
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                CupertinoTextField(
+                  placeholder: 'Почта',
+                  padding: const EdgeInsetsDirectional.symmetric(
+                      vertical: 20, horizontal: 16),
+                ),
+                Container(
+                  height: 1,
+                  color: Color(0xFFe0E6ED),
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                CupertinoTextField(
+                  obscureText: true,
+                  placeholder: 'Пароль',
+                  padding: const EdgeInsetsDirectional.symmetric(
+                      vertical: 20, horizontal: 16),
+                ),
+                // const SizedBox(
+                //   height: 327,
+                // ),
+              ],
             ),
-            CupertinoTextField(
-              placeholder: 'Телефон',
-            ),
-            CupertinoTextField(
-              placeholder: 'Почта',
-            ),
-            CupertinoTextField(
-              placeholder: 'Пароль',
-            ),
+            //  Spacer(),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
               child: CupertinoButton(
+                color: const Color(0xFF4631D2),
                 child: Text(
                   'Создать аккаунт',
                   style: TextStyle(fontWeight: FontWeight.bold),
